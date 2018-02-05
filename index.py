a274689bcbb6917d22b4a5f5ff3353e658a89574
Aamir Anwar <aamir.anwar021193@gmail.com>
2018-02-05 13:55:52 +0530

    Implemented queue using lists

--- index.py
+++ index.py
@@ -1,17 +1,6 @@
-import stack
+from queue import Queue
 print("Welcome to Python Lab!")
 
-myStack = stack.Stack()
+q = Queue()
 
-myStack.push(2)
-myStack.push(21)
-myStack.push(22)
-myStack.push(23)
-myStack.push(25)
 
-myStack.pop()
-myStack.pop()
-myStack.pop()
-myStack.pop()
-myStack.pop()
-myStack.pop()
---
+++ queue.py
@@ -0,0 +1,35 @@
+class Queue:
+    def __init__(self):
+        self.list = []
+
+
+    def isEmpty(self):
+        return True if len(self.list) <= 0 else False
+
+    def enqueue(self, data):
+        self.list.append(data)
+        print("Added {} to the queue".format(data))
+
+    def dequeue(self):
+        if not self.isEmpty():
+            item = self.list.pop(0)
+            print("Dequeued item {}".format(item))
+            return item
+        else:
+            print("Queue is empty!!")
+
+    def front(self):
+        if not self.isEmpty():
+            print("{} is at the front of the queue".format(self.list[0]))
+            return self.list[0]
+        else:
+            print("Queue is empty!!")
+
+    def end(self):
+        if not self.isEmpty():
+            last = self.list[len(self.list) - 1]
+            print("{} is at the end of the queue".format(last))
+            return last
+        else:
+            print("Queue is empty!!")
+
