97929cbc66b23995ac10f0d4b03b9cb84ba11063
Aamir Anwar <aamir.anwar021193@gmail.com>
2018-03-07 21:51:11 +0530

    Implemented fenwick tree

---
+++ fenwick_tree.py
@@ -0,0 +1,31 @@
+
+# Fenwick tree
+
+def getsum(i, BITTree):
+    s = 0
+
+    i = i + 1
+    while i > 0:
+        s += BITTree[i]
+        i -= i & (-i)
+    return s
+
+def updateBit(BITTree, n, i, v):
+    i += 1
+    while i <= n:
+        BITTree[i] += v
+        i += i & (-i)
+
+
+def construct(arr, n):
+    BITTree = [0]*(n + 1)
+    for i in range(n):
+        updateBit(BITTree, n, i, arr[i])
+    return BITTree
+
+freq = [2, 1, 1, 3, 2, 3, 4, 5, 6, 7, 8, 9]
+BITTree = construct(freq,len(freq))
+print("Sum of elements in arr[0..4] is " + str(getsum(4, BITTree)))
+freq[3] += 6
+updateBit(BITTree, len(freq), 3, 6)
+print("Sum of elements in arr[0..4] is " + str(getsum(4, BITTree)))
