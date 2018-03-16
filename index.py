c66cf916b5029bca239c150acd4c331b332aa125
Aamir Anwar <aamir.anwar021193@gmail.com>
2018-03-16 18:09:07 +0530

    Implemented towers of hanoi

--- index.py
+++ index.py
@@ -3,3 +3,20 @@ print("Welcome to Python Lab!")
 '''
 The Python Playground!
 '''
+
+def towersOfHanoi(n,from_stack,to_stack,aux_stack):
+    if n == 1:
+        print("Moved disc {} from {} to {}".format(n,from_stack, to_stack))
+    else:
+        towersOfHanoi(n-1, from_stack, aux_stack, to_stack)
+        print("Moved disc {} from {} to {}".format(n,from_stack, to_stack))
+        towersOfHanoi(n - 1, aux_stack, to_stack, from_stack)
+
+
+
+
+def test():
+    n = 4
+    towersOfHanoi(n,'A', 'B', 'C')
+
+test()
