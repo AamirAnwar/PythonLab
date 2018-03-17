1baa6520c9c05783c6d416f4d0cf59b218106309
Aamir Anwar <aamir.anwar021193@gmail.com>
2018-03-17 17:11:41 +0530

    Implemented array puzzles

--- index.py
+++ index.py
@@ -1,23 +1,4 @@
-
 print("Welcome to Python Lab!")
 '''
 The Python Playground!
 '''
-
-# For n disks, total 2^n – 1 moves are required.
-def towersOfHanoi(n,from_stack,to_stack,aux_stack):
-    if n == 1:
-        print("Moved disc {} from {} to {}".format(n,from_stack, to_stack))
-    else:
-        towersOfHanoi(n-1, from_stack, aux_stack, to_stack)
-        print("Moved disc {} from {} to {}".format(n,from_stack, to_stack))
-        towersOfHanoi(n - 1, aux_stack, to_stack, from_stack)
-
-
-
-
-def test():
-    n = 4
-    towersOfHanoi(n,'A', 'B', 'C')
-
-test()
